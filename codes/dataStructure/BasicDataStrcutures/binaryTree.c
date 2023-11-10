da626a487e485001580a717824c2b3d125397427
Rogério SA Junior <110107461+Burstys@users.noreply.github.com>
2023-11-10 19:10:17 -0300

    Binary Search Tree

--- codes/dataStructure/BasicDataStrcutures/binaryTree.c
+++ codes/dataStructure/BasicDataStrcutures/binaryTree.c
@@ -51,15 +51,82 @@ void printNodes (Node *t){
     }
 }
 
-int main(void){
-    Node *t = createNode(1, createNode(2, createEmptyNode(), createEmptyNode()), createNode(3, createEmptyNode(), createEmptyNode()));
-    printNodes(t);
-    printf("\n");
-    printf("%d", hasElement(t, 3));
-    printf("\n");
-    printf("%d", hasElement(t, 4));
-    printf("\n");
-    freeBTree(t);
-    return 0;
+//Binary search tree
+
+void bstPrint(Node *t){
+    if (t!=NULL){
+        bstPrint(t->left);
+        printf("%d", t->data);
+        bstPrint(t->right);
+    }
 }
 
+Node *bstHasElement(Node *t, int value){
+    if(t == NULL){
+        return NULL;
+    }
+    else if(t->data > value){
+        return bstHasElement(t->left, value);
+    }
+    else if(t->data < value){
+        return bstHasElement(t->right, value);
+    }
+    else{
+        return t;
+    }
+}
+
+Node *bstInsert(Node *t, int value){
+    if (t == NULL){
+        t = (Node*)malloc(sizeof(Node));
+        t->data = value;
+        t->left = t->right = NULL;
+    }
+
+    else if (value < t->data){
+        t->left = bstInsert(t->left, value);
+    }
+    else{ //v >= t->data
+        t->right = bstInsert(t->right, value);
+    }
+    return t;
+}
+
+Node *bstRemove(Node *t, int value){
+    if (t == NULL){
+        return NULL;
+    }
+    else if(t->data > value){
+        t->left = bstRemove(t->left, value);
+    }
+    else if(t->data < value){
+        t->right = bstRemove(t->right, value);
+    }
+    else{
+        if (t->left == NULL && t->right == NULL){
+            free(t);
+            t = NULL;
+        }
+
+        else if(t->left == NULL){
+            Node *aux = t;
+            t = t->right;
+            free(aux); 
+        }
+        else if(t->right == NULL){
+            Node *aux = t;
+            t = t->left;
+            free(aux); 
+        }
+        else{
+            Node *aux = t->left;
+            while (aux->right != NULL){
+                aux = aux->right;
+            }
+            t->data = aux->data;
+            aux->data = value;
+            t->left = bstRemove(t->left, value);
+        }
+    }
+    return t;
+}
